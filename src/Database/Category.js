befb965f4b40138d3c419e91c24a8a7ab3bd1705
Ritu Joshi <ritu.joshi@softobiz.com>
2023-10-11 14:02:02 +0530

    add categories

---
+++ src/Database/Category.js
@@ -0,0 +1,43 @@
+const Category =[
+    {
+        category: "General Knowledge",
+        value: 1
+    },
+    {
+        category: "Sports",
+        value: 2
+    },
+    {
+        category: "Books",
+        value: 3
+    },
+    {
+        category: "Geography",
+        value:4
+    },
+    {
+        category: "History",
+        value: 5
+    },
+    {
+        category: "Politics",
+        value: 6
+    },
+    {
+        category: "Science and Nature",
+        value: 7
+    },
+    {
+        category: "Computer",
+        value: 8
+    },
+    {
+        category: "Mathematics",
+        value: 9
+    },
+    {
+        category: "Mythology",
+        value: 10
+    },
+];
+export default Category;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import bannerImg from "../assets/images/question2.png"
+import "../../src/App.css"
+import { Button, MenuItem, TextField } from '@mui/material'
+import Category from '../Database/Category'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="home-content">
+    
+    <div className="quiz-setup">
+      <span className="quiz-setting"> Enter the information</span>
+      
+      <div className="add-inputs">
+        <TextField label="Enter your Name" variant='outlined' className="input-field" />
+        <TextField select label="Select Category" variant='outlined' className="input-field">
+        {
+          Category.map((cat) => (
+            <MenuItem key={cat.category} value={cat.value}>
+              {cat.category}
+            </MenuItem>
+          ))}
+      </TextField>
+          
+          <TextField select label="Select Difficulty" variant='outlined' className="input-field">
+          <MenuItem key="Easy" value="easy">
+            Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+            Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+            Hard
+            </MenuItem>
+          </TextField>
+            <Button variant='contained' color='primary' size='large' className="input-field">
+              Start Quiz
+            </Button>
+
+
+      </div>
+
+
+    </div>
+
+    <div className="banner-img">
+      <img src={bannerImg} alt="Banner-Image" />
+    </div>
+
+    </div>
+    </>
   )
 }
 
