1f36e752df2a210b08f97488342d745d0504b6f4
Ritu Joshi <ritu.joshi@softobiz.com>
2023-10-11 10:20:30 +0530

    Route setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import Result from './pages/Result';
 
 function App() {
   return (
    <BrowserRouter>
-    <div className="app"  >
+    <div className="app">
       <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/quiz" element={<Quiz/>}/>
+        <Route path="/result" element={<Result/>}/>
+      </Routes>
       
     </div>
     <Footer />
